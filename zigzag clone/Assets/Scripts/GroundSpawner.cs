9e369ab67504f1b1dcde5ecc1fcbf60f108944f1
Emre Boztaş <105316939+EmreBoztas@users.noreply.github.com>
2023-01-23 23:05:55 +0300

    Random path generation.

---
+++ zigzag clone/Assets/Scripts/GroundSpawner.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject last_ground;
+    Vector3 direction;
+    private int first = 0;
+    private int second = 0;
+
+    void Start()
+    {
+        for(int i = 0; i < 10 ;i++)
+        {
+            ground_spawner();
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void ground_spawner()
+    {
+        
+        
+        if(((Random.Range(0,2) == 0) && first < 2) || second > 2)
+        {
+            direction = Vector3.left;
+            first++;
+            second = 0;
+        }
+        else
+        {
+            direction = Vector3.forward;
+            second++;
+            first = 0;
+        }
+
+
+        last_ground = Instantiate(last_ground, last_ground.transform.position+direction,last_ground.transform.rotation); 
+    }
+}
